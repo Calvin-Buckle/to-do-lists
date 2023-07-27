27c666d0b0584ee21c0d6fdd6b4c59c7acb57cd0
Calvin Buckle <ccccbuckle@gmail.com>
2023-07-27 14:43:03 +0200

    make the to do form's submit button save the to do item

--- src/todo.js
+++ src/todo.js
@@ -2,7 +2,7 @@ function toDoAdd(){
 
 let toDoLibrary = []
 
-function toDoList(){
+function toDoList(toDoName){
     this.toDoName = toDoName
 }
 
@@ -21,10 +21,48 @@ function toDoList(){
 
 const addToDoButton = document.createElement('button')
 addToDoButton.classList.add('addToDO')
-addToDoButton.innerHTML = 'Add to DO'
+addToDoButton.innerHTML = 'Add to DO';
 
 const toDoListBody = document.querySelector('.to-do-list-body')
 toDoListBody.appendChild(addToDoButton)
+
+
+
+
+
+
+
+
+//die een
+function render(){
+    let toDoLibraryEl = document.querySelector('.to-do-list-body');
+    toDoLibraryEl.innerHTML = "";
+    for (let i = 0; i < toDoLibrary.length; i++){
+    let toDo = toDoLibrary[i];
+    let toDoEl = document.createElement("div");
+    toDoEl.innerHTML = `<p>${toDo.toDoName}<p>`
+    toDoLibraryEl.appendChild(toDoEl)}}
+
+
+
+
+const toDoSubmit = document.querySelector('#to-Do-Submit')
+toDoSubmit.addEventListener('click', (e) =>{
+
+
+    addToDoToLibrary();
+    e.preventDefault();
+    function addToDoToLibrary(){
+        let toDoName = document.querySelector('#name-to-do').value
+        let newToDoList = new toDoList(toDoName)
+        toDoLibrary.push(newToDoList)
+        render();
+        addProjectForm.style.display = 'none';
+    
+    }
+    
+
+})
 
 
 
